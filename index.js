c0bc272a2468ed16f72d972c0a43df8916e0c5f8
tobySolutions <adedejitobiloba7@gmail.com>
2024-11-30 13:10:52 +0100

    feat: add finished state machine

---
+++ index.js
@@ -0,0 +1,76 @@
+function createMachine(stateMachineDefinition) {
+  const machine = {
+    value: stateMachineDefinition.initialState,
+    transition(currentState, event) {
+      const currentStateDefinition = stateMachineDefinition[currentState];
+      const destinationTransition = currentStateDefinition.transitions[event];
+
+      if (!destinationTransition) {
+        return;
+      }
+
+      const destinationState = destinationTransition.target;
+      const destinationStateDefinition =
+        stateMachineDefinition[destinationState];
+
+      destinationTransition.action();
+      currentStateDefinition.actions.onExit();
+      destinationStateDefinition.actions.onEnter();
+
+      machine.value = destinationState;
+
+      return machine.value;
+    },
+  };
+  return machine;
+}
+
+// here's how we'll create the state machine
+const machine = createMachine({
+  initialState: "off",
+  off: {
+    actions: {
+      onEnter() {
+        console.log("off: onEnter");
+      },
+      onExit() {
+        console.log("off: onExit");
+      },
+    },
+    transitions: {
+      switch: {
+        target: "on",
+        action() {
+          console.log('transition action for "switch" in "off" state');
+        },
+      },
+    },
+  },
+  on: {
+    actions: {
+      onEnter() {
+        console.log("on: onEnter");
+      },
+      onExit() {
+        console.log("on: onExit");
+      },
+    },
+    transitions: {
+      switch: {
+        target: "off",
+        action() {
+          console.log('transition action for "switch" in "on" state');
+        },
+      },
+    },
+  },
+});
+
+let state = machine.value;
+console.log(`current state: ${state}`);
+
+state = machine.transition(state, "switch");
+console.log(`current state: ${state}`); // current state: on
+
+state = machine.transition(state, "switch");
+console.log(`current state: ${state}`); // current state: off
